a5e7e93541dcb5ea41c18be880be6cb4b7f72e98
kphonasa <pwnasa@gmail.com>
2017-04-04 12:19:32 -0500

    Add files via upload

---
+++ webRefresh.py
@@ -0,0 +1,14 @@
+#Constantly refreshes page at a rate of choice
+from selenium import webdriver
+import time
+import urllib
+import urllib2
+
+x=input("Enter the URL you wish to refresh:")
+r=input("Enter the number of seconds till next refresh:")
+r=int(r)
+driver = webdriver.Firefox()
+driver.get(x)
+while True:
+    time.sleep(r)
+    driver.refresh()
