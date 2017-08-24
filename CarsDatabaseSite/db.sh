f4f441a3affe504845c9d4bbe82ba4eda190330d
kphonasa <pwnasa@gmail.com>
2017-08-23 20:16:14 -0700

    fixed car database gitignore

--- CarsDatabaseSite/db.sh
+++
@@ -1,6 +0,0 @@
-[kphonasa@serv42 hw3]$ more db.sh
-#!/bin/sh
-
-# Change DBPASSWORD, DBUSER, DBHOST and DBNAME to match the values
-# your mysql_db_info file on the webdev server.
-mysql --password='MT_G.noQxVHv' --user='kphonasa' --host='dbdev.cs.uiowa.edu' 'db_kphonasa'
